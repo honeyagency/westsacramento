f2009fede2122ed4e0f163902227dc19e32d0b65
joshre <joshre@me.com>
2019-01-09 09:39:04 -0800

    Docs

--- library/buscemi.php
+++ library/buscemi.php
@@ -8,7 +8,7 @@ function bc_dashboard_widget_function()
 }
 function bc_add_dashboard_widgets()
 {
-    wp_add_dashboard_widget('wp_dashboard_widget', 'Buscemi Docs', 'bc_dashboard_widget_function');
+    wp_add_dashboard_widget('wp_dashboard_widget', 'Wordpress Overview', 'bc_dashboard_widget_function');
 }
 add_action('wp_dashboard_setup', 'bc_add_dashboard_widgets');
 
